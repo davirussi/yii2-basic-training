8baf2064b971731dbff02c2207720c16f1aac8e1
DaviR <davirussi@gmail.com>
2016-03-31 14:30:14 -0300

    changing label descricao to descrição

--- models/Tag.php
+++ models/Tag.php
@@ -43,7 +43,7 @@ class Tag extends \yii\db\ActiveRecord
         return [
             'id' => 'ID',
             'nome' => 'Nome',
-            'descricao' => 'Descricao',
+            'descricao' => 'Descrição',
         ];
     }
 
