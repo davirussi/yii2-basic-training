a90c63eccd0f217143d2782fb07c1752b20bb884
DaviR <davirussi@gmail.com>
2016-03-31 14:30:40 -0300

    adding the missing field to _form tag

--- views/tag/_form.php
+++ views/tag/_form.php
@@ -13,6 +13,8 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'nome')->textInput(['maxlength' => true]) ?>
+    
+    <?= $form->field($model, 'descricao')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
