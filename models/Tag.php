98e23e6003d0261d0969222d12909e79729ed8ca
DaviR <davirussi@gmail.com>
2016-03-31 14:14:25 -0300

    adding basic class generated by gii

---
+++ models/Tag.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "my_tag".
+ *
+ * @property integer $id
+ * @property string $nome
+ * @property string $descricao
+ *
+ * @property MyPostTag[] $myPostTags
+ * @property Post[] $posts
+ */
+class Tag extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'my_tag';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['nome'], 'required'],
+            [['nome', 'descricao'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nome' => 'Nome',
+            'descricao' => 'Descricao',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMyPostTags()
+    {
+        return $this->hasMany(MyPostTag::className(), ['tag_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPosts()
+    {
+        return $this->hasMany(Post::className(), ['id' => 'post_id'])->viaTable('my_post_tag', ['tag_id' => 'id']);
+    }
+}
